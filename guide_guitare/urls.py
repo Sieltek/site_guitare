d6b6495e4c1f960495a43dd6e67d74f5de18714c
SieLteK <marco38000@gmail.com>
2020-01-17 16:40:26 +0100

    Feat(guide_guitare): Add templates

--- guide_guitare/urls.py
+++ guide_guitare/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, guitares
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="index"),
+    path('guitares', guitares, name="guitares"),
 ]
--- guide_guitare/views.py
+++ guide_guitare/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'guide_guitare/index.html')
+
+
+def guitares(request):
+
+    return render(request, 'guide_guitare/guitares.html')
